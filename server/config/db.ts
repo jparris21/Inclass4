ee7ac4c3b2a9e4d76784371aefccde00e643bb1a
jparris21.github.io <jadenparris@hotmail.com>
2024-04-17 22:58:34 -0400

    updated db

--- server/config/db.ts
+++ server/config/db.ts
@@ -6,9 +6,10 @@ if(LOCAL){
     URI = "mongodb://localhost/contacts";
     HostName = "localhost"
 }else{
-    URI = "mongodb+srv://ramiyan2185:ravi1967@cluster0.lwt5o6f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    URI = "mongodb+srv://jadenparris:Cupcake$101@cluster0.6neikt7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
     HostName = "MongoDB Atlas"
 }
+
 
 export { HostName, URI }
 export const SessionSecret = "INFT2202SessionSecret";
